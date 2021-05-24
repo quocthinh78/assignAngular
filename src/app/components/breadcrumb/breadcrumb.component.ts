17cf75bd07895217f9f4f0f35634824489652f8c
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-05-24 18:22:29 +0700

    u

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {ActivatedRoute} from '@angular/router'
+import { Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
 })
 export class BreadcrumbComponent implements OnInit {
 
-  public href: string = "";
+  public href: any = "";
 
-    constructor(private router: Router) {}
+  constructor(private router: Router) {
 
+    this.router.events.subscribe((event: Event) => {
+
+        if (event instanceof NavigationEnd) {
+          console.log(event)
+          let events = event.urlAfterRedirects;
+          let arr = events.split('/');
+          let arrHref = arr.splice(1);
+          this.href = arrHref;
+
+        }
+    });
+
+}
     ngOnInit() {
-        this.href = this.router.url;
-        console.log(this.router);
+        this.spreadHref();
     }
-
+    spreadHref(){
+      // let arr = this.href.split('/');
+      // console.log(this.href)
+    }
 }
